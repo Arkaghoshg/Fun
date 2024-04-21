3d97c2aec69514adafbfc620586f2e0c4f5bb3c9
Arkaghoshg <144145423+Arkaghoshg@users.noreply.github.com>
2024-04-21 16:01:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("yesButton").addEventListener("click", function() {
+    alert("I like you too!");
+});
+
+document.getElementById("noButton").addEventListener("click", function() {
+    alert("I'll try to change myself.");
+});
